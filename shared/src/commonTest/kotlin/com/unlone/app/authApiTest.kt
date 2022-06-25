86c5e135f27d182b5ad91114500f9af0c23f505a
WesleyHung <>
2022-06-25 13:56:35 +0800

    create ktor client test for auth api

--- shared/src/commonTest/kotlin/com/unlone/app/authApiTest.kt
+++ shared/src/commonTest/kotlin/com/unlone/app/authApiTest.kt
@@ -12,23 +12,25 @@ import kotlin.test.Test
 // todo: still studying
 class AuthApiTest {
 
+    private val mockEngine = MockEngine { request ->
+        // todo: what does it do?
+        respond(
+            content = ByteReadChannel("""{"ip":"127.0.0.1"}"""),
+            status = HttpStatusCode.OK,
+            headers = headersOf(HttpHeaders.ContentType, "application/json")
+        )
+    }
+    private val authApiService = AuthApiService(mockEngine)
+
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun sampleClientTest() {
         runBlocking {
-            val mockEngine = MockEngine { request ->
-                respond(
-                    content = ByteReadChannel("""{"ip":"127.0.0.1"}"""),
-                    status = HttpStatusCode.OK,
-                    headers = headersOf(HttpHeaders.ContentType, "application/json")
-                )
-            }
-
-            val authApiService = AuthApiService(mockEngine)
-
             // if it doesn't crash, the test succeed
             authApiService.authenticate("12345")
         }
     }
+
+
 
 }
