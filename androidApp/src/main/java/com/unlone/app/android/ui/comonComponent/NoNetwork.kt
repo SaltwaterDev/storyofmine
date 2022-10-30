6101491f0d23a30a11024efcebb7cfff5e101795
WesleyHung <unlonely2021@gmail.com>
2022-10-30 10:43:44 +0800

    create NoNetwork.kt; ui fix StoryDetailViewModel.kt

---
+++ androidApp/src/main/java/com/unlone/app/android/ui/comonComponent/NoNetwork.kt
@@ -0,0 +1,44 @@
+package com.unlone.app.android.ui.comonComponent
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.WifiOff
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NoNetworkScreen() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 60.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.WifiOff,
+                contentDescription = null,
+                modifier = Modifier.requiredSize(180.dp)
+            )
+            Text(
+                text = "Network unavailable\n" +
+                        "Please check your network state",
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(vertical = 20.dp)
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun NoNetworkScreenPreview() {
+    NoNetworkScreen()
+}
--- androidApp/src/main/java/com/unlone/app/android/viewmodel/StoryDetailViewModel.kt
+++ androidApp/src/main/java/com/unlone/app/android/viewmodel/StoryDetailViewModel.kt
@@ -12,10 +12,10 @@ import kotlinx.coroutines.launch
 
 data class StoryDetailUiState(
     val pid: String = "",
-    val title: String = "Title",
-    val content: String = "Content",
+    val title: String = "\b".repeat(5),
+    val content: String = "\b".repeat(10),
     val authorId: String = "",
-    val topic: String = "Topic",
+    val topic: String = "\b".repeat(5),
     val createdDate: String = "",
     val comments: List<Comment> = listOf(),
     val isSelfWritten: Boolean = false,
