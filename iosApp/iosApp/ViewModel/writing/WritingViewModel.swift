c67b8b950ef10eac16bdab4a2fc77a0d6f690133
WesleyHung <unlonely2021@gmail.com>
2023-03-14 22:32:52 +0800

    fix ios build

--- iosApp/iosApp/ViewModel/writing/WritingViewModel.swift
+++ iosApp/iosApp/ViewModel/writing/WritingViewModel.swift
@@ -132,7 +132,7 @@ class WritingViewModel: ObservableObject {
         Task {
             do {
                 guard !self.title.isEmpty else { return }
-                try await saveDraftUseCase.invoke(id: self.currentDraftId, title: self.title, body: self.content)
+                try await saveDraftUseCase.invoke(draftId: self.currentDraftId, title: self.title, body: self.content, shouldCreateNewVersion: false)
             } catch{ print(error) }
         }
     }
